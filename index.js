82b6097f5e54dfcb72083cec1ae78f9717a0e8f5
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-20 11:50:07 -0400

    added find funrniture by model function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addNewFurniture, getAllFurniture } from './src/furniture.js';
+import { addNewFurniture, getAllFurniture, findFurnitureByModel } from './src/furniture.js';
 
 const PORT = 3030
 const app = express();
@@ -8,5 +8,6 @@ app.use(express.json());
 app.get("/", (req, res) => res.send('working'))
 app.post("/furniture", addNewFurniture)
 app.get("/furniture", getAllFurniture)
+app.get("/furniture/:search", findFurnitureByModel)
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}...`))
--- src/furniture.js
+++ src/furniture.js
@@ -16,3 +16,12 @@ export async function getAllFurniture(req, res) {
   const collection = await db.collection("furniture").find().toArray()
   res.send(collection)
 }
+
+export async function findFurnitureByModel(req, res) {
+  const db = dbConnect()
+  const { search } = req.params
+  const collection = await db.collection("furniture")
+    .find({ model: search })
+    .toArray()
+  res.send(collection)
+}
