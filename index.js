fcf3584165508f21c2237d1a08c131a95d6e8a9c
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-20 10:49:44 -0400

    formatting updates

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3030
 const app = express();
 app.use(express.json());
 
-app.get('/', (req,res) => res.send('working'))
+app.get('/', (req, res) => res.send('working'))
 app.post("/furniture", addNewFurniture)
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}...`))
--- src/furniture.js
+++ src/furniture.js
@@ -1,12 +1,12 @@
 import { dbConnect } from "./dbConnect.js";
 
-export async function addNewFurniture (req, res) {
+export async function addNewFurniture(req, res) {
   const newFurniture = req.body
   const db = dbConnect()
   await db.collection("furniture").insertOne(newFurniture)
     .catch(err => {
       res.status(500).send(err)
-      return 
+      return
     })
   res.status(201).send({ message: "New Furniture Added." })
 }
